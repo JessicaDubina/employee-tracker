edc5efb5cfeb85972832f0a816dfd5cc3bd2869d
jdub <jessicaclaing@gmail.com>
2024-01-31 21:01:51 -0800

    setup initial schema file and staff_db. connected server to the database

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,3 +8,13 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//connect to the database
+const db = mysql.createConnection(
+    {
+      host: '127.0.0.1',
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
+    },
+    console.log(`Connected to the books_db database.`)
+  );
