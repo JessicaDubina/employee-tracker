8e59813effdfac09e45725fbf9c3d7a2685ea781
jdub <jessicaclaing@gmail.com>
2024-01-31 21:20:11 -0800

    created sql tables for department, roles, and employees

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,31 @@ DROP DATABASE IF EXISTS staff_db;
 CREATE DATABASE staff_db;
 
 USE staff_db;
+
+CREATE TABLE department (
+    id INT NOT NULL,
+    name VARCHAR(25) NOT NULL
+    PRIMARY KEY (id)
+)
+
+CREATE TABLE roles (
+    job_title VARCHAR(30) NOT NULL,
+    id INT NOT NULL,
+    department_id INT,
+    salary INT NOT NULL
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+    PRIMARY KEY (id)
+)
+
+CREATE TABLE employees (
+    id INT NOT NULL,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    role_id INT
+    manager VARCHAR(30) NOT NULL,
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    ON DELETE SET NULL
+)
