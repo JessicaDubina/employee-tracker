f46d3fa4cd048f4ac8cf481ad4f4e332ab8aa276
jdub <jessicaclaing@gmail.com>
2024-02-01 09:50:01 -0800

    wrapped main menu questions into an function

--- api/index.js
+++ api/index.js
@@ -3,30 +3,27 @@ const inquirer = require('inquirer');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('This worked!');
-});
+const mainMenu = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'mainMenuSelect',
+            message: 'What would you like to do?',
+            choices: [
+                'View all departments', 
+                'View all roles', 
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee role'
+            ]
+        }
+    ])
+}
 
+mainMenu();
+//turn this into a (req, res) based on selection. 
 
-
-inquirer.prompt([
-    {
-        type: 'list',
-        name: 'mainMenuSelect',
-        message: 'What would you like to do?',
-        choices: [
-            'View all departments', 
-            'View all roles', 
-            'View all employees',
-            'Add a department',
-            'Add a role',
-            'Add an employee',
-            'Update an employee role'
-        ]
-    }
-])
-
-//firs tneed to populate list of options for user to choose from
-//then use answer to go to next screen
 
 module.exports = app;
